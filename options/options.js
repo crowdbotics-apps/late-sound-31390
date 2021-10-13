4673099bb83a5fb609b06c8a844971c4f92feb0f
Crowdbotics <team@crowdbotics.com>
2021-10-13 19:41:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "late_sound_31390",
+  url: "https://late_sound_31390.botics.co",
+  api: "https://late_sound_31390.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
